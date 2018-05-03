294e1b0687855acd209026d711e16a9f0332f16a
Selma <asakab@live.fr>
2018-05-03 20:04:07 +0200

    alleeer

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,12 @@ def new
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      log_in user
-      redirect_to user
+     user = User.find_by(name: params[:session][:name].downcase)
+    if user && user.authenticate(params[:session][:name])
+    	log_in user redirect_to user 
+      # Log the user in and redirect to the user's show page.
     else
-      flash.now[:danger] = 'Invalid email/password combination'
+        flash.now[:danger] = 'Wrong username !' # Not quite right!
       render 'new'
     end
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,19 @@ class UsersController < ApplicationController
 
   def create
   	@user = User.create(name: params['name'])
+  
+  #	if @user.save
+  #	   log_in @user 
+  #	   flash[:success] = "Welcome to the Sample App!"
+   #    redirect_to @user
+
+    #else
+
+    #  render 'new'
+
   end
+
+
 
   def show
   	@user = User.create(name: params['name'])
